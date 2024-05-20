b0f8693c3fc01cb6b1925ba9f7d3e469dc8c76d5
Ewan-Brown <ewanbrown2000@gmail.com>
2024-05-20 08:49:31 -0400

    Initial commit

---
+++ src/main/kotlin/world/Game.kt
@@ -0,0 +1,13 @@
+package world
+
+class World {
+    private val entityList : List<Entity> = emptyList()
+
+    fun update() {
+        for (entity in entityList) {
+            
+        }
+    }
+//        for(entity in entityList){}
+
+}
