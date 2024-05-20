d42c09b71e2927604d1f16bc94c02e3ddc18e741
Ewan-Brown <ewanbrown2000@gmail.com>
2024-05-20 08:53:01 -0400

    Init Commit 2

--- src/main/kotlin/world/Game.kt
+++ src/main/kotlin/world/Game.kt
@@ -1,13 +1,15 @@
 package world
 
-class World {
-    private val entityList : List<Entity> = emptyList()
+import org.dyn4j.world.World
+import world.entity.Entity
 
+class Game {
+    private val entityList : List<Entity> = emptyList()
+    private val world : World<Entity> = World();
     fun update() {
         for (entity in entityList) {
-            
+
         }
     }
-//        for(entity in entityList){}
 
 }
