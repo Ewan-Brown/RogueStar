7fcb0c560588e9c04f3c9b9a422295d53a0f61bd
Ewan Brown <ewanbrown2000@gmail.com>
2024-06-27 20:14:14 -0400

    Deleted a bunch of nonsense

--- src/main/kotlin/world/rendering/Renderable.kt
+++
@@ -1,9 +0,0 @@
-package world.rendering
-
-import org.dyn4j.geometry.Vector2
-
-interface Renderable {
-    fun getRenderableElements() : List<RenderableElement>
-    fun getWorldPos() : Vector2
-    fun getAbsoluteAngle() : Double
-}
