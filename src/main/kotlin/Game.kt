86218e735328edf353c449044a24c95b59632eac
Ewan-Brown <ewanbrown2000@gmail.com>
2024-07-03 19:33:57 -0400

    Cleaned up PhysicsEntity and DrawableInstance
    PhysicsEntity is now constructed with model immediately. Yet to see if collision works correct.
    DrawableInstance is now 'Component' - an instance of a Model with an associated unique Transform

--- src/main/kotlin/Game.kt
+++ src/main/kotlin/Game.kt
@@ -17,62 +17,49 @@ fun main() {
     val models = listOf(Model.TRIANGLE, Model.SQUARE1)
     val gui = Graphics(models)
     val testEntity = SimpleEffectsEntity(Vector2(0.0,0.0), model = Model.TRIANGLE)
-    val testPhysicsEntity = PhysicsEntity()
-    testPhysicsEntity.setModels(listOf(Pair(Model.SQUARE1, Graphics.Transform(Vector2(0.0,0.0), 0.0f))))
+    val testPhysicsEntity = PhysicsEntity(listOf(Component(Model.SQUARE1, Graphics.Transform(Vector2(0.0,0.0), 0.0f))))
+//    testPhysicsEntity.setModels(listOf(Pair(Model.SQUARE1, Graphics.Transform(Vector2(0.0,0.0), 0.0f))))
 
     gui.setup()
     while(true){
         Thread.sleep(15)
         testEntity.position = testEntity.position.add(0.01,0.0)
+        testEntity.angle += 0.01f;
         val modelDataMap = hashMapOf<Model, MutableList<Graphics.Transform>>()
         for (model in models) {
             modelDataMap[model] = mutableListOf()
         }
-        var drawableThings : List<DrawableInstance> = testEntity.getDrawableInstances() + testPhysicsEntity.getDrawableInstances()
+        var drawableThings : List<Component> = testEntity.getDrawableInstances() + testPhysicsEntity.getDrawableInstances()
         for (drawable in drawableThings) {
             modelDataMap[drawable.model]!!.add(drawable.transform);
         }
         gui.updateDrawables(modelDataMap)
+        physicsWorld.update(1.0)
     }
 }
 
 private interface DrawableProvider{
-    fun getDrawableInstances() : List<DrawableInstance>
+    fun getDrawableInstances() : List<Component>
 }
 
-private data class DrawableInstance(val model: Model, val transform: Graphics.Transform)
+private data class Component(val model: Model, val transform: Graphics.Transform)
 
-private class PhysicsEntity : AbstractPhysicsBody(), DrawableProvider {
-    private var models: List<Pair<Model, Graphics.Transform>>? = null
-    private val requiredModels : MutableList<Model> = mutableListOf()
+private class PhysicsEntity(val components: List<Component>) : AbstractPhysicsBody(), DrawableProvider {
 
-    override fun getDrawableInstances(): List<DrawableInstance> {
-        val result: MutableList<DrawableInstance> = ArrayList()
+    override fun getDrawableInstances(): List<Component> {
+        val result: MutableList<Component> = ArrayList()
 
         val entityAngle = getTransform().rotationAngle.toFloat()
         val entityPos = this.worldCenter
 
-        for (component in models!!) {
-            val newPos = component.component2().position.copy().rotate(entityAngle.toDouble()).add(entityPos)
-            val newAngle = entityAngle + component.component2().angle
-            result.add(DrawableInstance(component.component1(),Graphics.Transform(newPos, newAngle)))
+        for (component in components) {
+            val newPos = component.transform.position.copy().rotate(entityAngle.toDouble()).add(entityPos)
+            val newAngle = entityAngle + component.transform.angle
+            result.add(Component(component.model,Graphics.Transform(newPos, newAngle)))
         }
 
         return result
     }
-
-    //Why isn't this just part of the constructor
-    fun setModels(models: List<Pair<Model,Graphics.Transform>>) {
-        for (model in models) {
-            model.second.position.subtract(getMass().center)
-            if(!requiredModels.contains(model.first)){
-                requiredModels.add(model.first)
-            }
-        }
-        this.models = models
-    }
-
-
 }
 
 private class PhysicsWorld : AbstractPhysicsWorld<PhysicsEntity, WorldCollisionData<PhysicsEntity>>(){
@@ -90,8 +77,8 @@ private class SimpleEffectsEntity(
     var angularVelocity: Float = 0.0f,
     val model: Model,
 ) : EffectsEntity(){
-    override fun getDrawableInstances(): List<DrawableInstance> {
-        return return mutableListOf(DrawableInstance(model, Graphics.Transform(position.copy(), angle)))
+    override fun getDrawableInstances(): List<Component> {
+        return return mutableListOf(Component(model, Graphics.Transform(position.copy(), angle)))
     }
 
 }
