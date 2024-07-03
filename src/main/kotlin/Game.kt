259546d024f326e1f3881b13e13156dd3eebafe5
Ewan-Brown <ewanbrown2000@gmail.com>
2024-07-03 19:47:17 -0400

    Setup the physics world + added model to physical entities

--- src/main/kotlin/Game.kt
+++ src/main/kotlin/Game.kt
@@ -3,6 +3,8 @@ import org.dyn4j.collision.CollisionItem
 import org.dyn4j.collision.CollisionPair
 import org.dyn4j.dynamics.AbstractPhysicsBody
 import org.dyn4j.dynamics.BodyFixture
+import org.dyn4j.geometry.MassType
+import org.dyn4j.geometry.Polygon
 import org.dyn4j.geometry.Vector2
 import org.dyn4j.world.AbstractPhysicsWorld
 import org.dyn4j.world.WorldCollisionData
@@ -16,15 +18,23 @@ fun main() {
     val effectsWorld = EffectsWorld();
     val models = listOf(Model.TRIANGLE, Model.SQUARE1)
     val gui = Graphics(models)
+
     val testEntity = SimpleEffectsEntity(Vector2(0.0,0.0), model = Model.TRIANGLE)
+
+    physicsWorld.setGravity(0.0,0.0)
+
     val testPhysicsEntity = PhysicsEntity(listOf(Component(Model.SQUARE1, Graphics.Transform(Vector2(0.0,0.0), 0.0f))))
-//    testPhysicsEntity.setModels(listOf(Pair(Model.SQUARE1, Graphics.Transform(Vector2(0.0,0.0), 0.0f))))
+    testPhysicsEntity.setMass(MassType.NORMAL)
+    physicsWorld.addBody(testPhysicsEntity)
 
     gui.setup()
     while(true){
         Thread.sleep(15)
+
         testEntity.position = testEntity.position.add(0.01,0.0)
         testEntity.angle += 0.01f;
+        testPhysicsEntity.applyForce(Vector2(1.0,1.0))
+
         val modelDataMap = hashMapOf<Model, MutableList<Graphics.Transform>>()
         for (model in models) {
             modelDataMap[model] = mutableListOf()
@@ -44,7 +54,26 @@ private interface DrawableProvider{
 
 private data class Component(val model: Model, val transform: Graphics.Transform)
 
-private class PhysicsEntity(val components: List<Component>) : AbstractPhysicsBody(), DrawableProvider {
+private class PhysicsEntity : AbstractPhysicsBody, DrawableProvider {
+
+    val components: List<Component>
+
+    constructor(components: List<Component>) : super() {
+        this.components = components
+
+        for (component in components) {
+            val vertices = arrayOfNulls<Vector2>(component.model.points)
+            for (i in vertices.indices) {
+                vertices[i] = component.model.asVectorData[i].copy()
+            }
+            val v = Polygon(*vertices)
+            v.translate(component.transform.position.copy())
+            v.rotate(component.transform.angle.toDouble())
+            val f = BodyFixture(v)
+            this.addFixture(f)
+        }
+
+    }
 
     override fun getDrawableInstances(): List<Component> {
         val result: MutableList<Component> = ArrayList()
@@ -64,6 +93,7 @@ private class PhysicsEntity(val components: List<Component>) : AbstractPhysicsBo
 
 private class PhysicsWorld : AbstractPhysicsWorld<PhysicsEntity, WorldCollisionData<PhysicsEntity>>(){
     override fun createCollisionData(pair: CollisionPair<CollisionItem<PhysicsEntity, BodyFixture>>?): WorldCollisionData<PhysicsEntity>? {
+        println("oh it's a collision")
         return null;
     }
 }
