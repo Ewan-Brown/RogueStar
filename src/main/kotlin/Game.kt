a7142d87fbf98abb87199e8ea20d1b338779c9e7
Ewan-Brown <ewanbrown2000@gmail.com>
2024-07-03 10:29:17 -0400

    Graphics now receives individual frame data as opposed to having leaked references to the entities themselves

--- src/main/kotlin/Game.kt
+++ src/main/kotlin/Game.kt
@@ -1,4 +1,4 @@
-import Graphics.DrawableThing
+import Graphics.DrawableInstance
 import Graphics.Model
 import com.jogamp.opengl.Threading.Mode
 import jogl.instanced.HelloTriangle_12_correct_dyn4j_bodies
@@ -21,22 +21,26 @@ fun main() {
     val testEntity = SimpleEffectsEntity(Vector2(0.0,0.0), model = Model.TRIANGLE)
     val testPhysicsEntity = PhysicsEntity();
     testPhysicsEntity.setModels(listOf(Pair(Model.SQUARE1, Graphics.Transform(Vector2(0.0,0.0), 0.0f))))
-    val drawableThings = listOf(testEntity, testPhysicsEntity);
-    gui.updateDrawables(drawableThings)
+
     gui.setup()
     while(true){
         Thread.sleep(15)
         testEntity.position = testEntity.position.add(0.01,0.0)
+        var drawableThings : List<DrawableInstance> = testEntity.getDrawableInstances() + testPhysicsEntity.getDrawableInstances();
+        gui.updateDrawables(drawableThings)
     }
 }
 
-class PhysicsEntity : AbstractPhysicsBody(), DrawableThing {
-    private var models: List<Pair<Model, Graphics.Transform>>? =
-        null
+interface DrawableProvider{
+    fun getDrawableInstances() : List<DrawableInstance>
+}
+
+class PhysicsEntity : AbstractPhysicsBody(), DrawableProvider {
+    private var models: List<Pair<Model, Graphics.Transform>>? = null
     private val requiredModels : MutableList<Model> = mutableListOf()
 
-    override fun getTransformedComponents(): MutableList<Pair<Model, Graphics.Transform>> {
-        val result: MutableList<Pair<Model,Graphics.Transform>> = ArrayList()
+    override fun getDrawableInstances(): List<DrawableInstance> {
+        val result: MutableList<DrawableInstance> = ArrayList()
 
         val entityAngle = getTransform().rotationAngle.toFloat()
         val entityPos = this.worldCenter
@@ -44,15 +48,30 @@ class PhysicsEntity : AbstractPhysicsBody(), DrawableThing {
         for (component in models!!) {
             val newPos = component.component2().position.copy().rotate(entityAngle.toDouble()).add(entityPos)
             val newAngle = entityAngle + component.component2().angle
-            result.add(Pair(component.component1(),Graphics.Transform(newPos, newAngle)))
+            result.add(DrawableInstance(component.component1(),Graphics.Transform(newPos, newAngle)))
         }
 
         return result
     }
 
-    override fun getRequiredModels(): MutableList<Model> {
-        return requiredModels
-    }
+//    override fun getTransformedComponents(): MutableList<Pair<Model, Graphics.Transform>> {
+//        val result: MutableList<Pair<Model,Graphics.Transform>> = ArrayList()
+//
+//        val entityAngle = getTransform().rotationAngle.toFloat()
+//        val entityPos = this.worldCenter
+//
+//        for (component in models!!) {
+//            val newPos = component.component2().position.copy().rotate(entityAngle.toDouble()).add(entityPos)
+//            val newAngle = entityAngle + component.component2().angle
+//            result.add(Pair(component.component1(),Graphics.Transform(newPos, newAngle)))
+//        }
+//
+//        return result
+//    }
+//
+//    override fun getRequiredModels(): MutableList<Model> {
+//        return requiredModels
+//    }
 
     //Why isn't this just part of the constructor
     fun setModels(models: List<Pair<Model,Graphics.Transform>>) {
@@ -64,18 +83,17 @@ class PhysicsEntity : AbstractPhysicsBody(), DrawableThing {
         }
         this.models = models
     }
+
+
 }
 
 class PhysicsWorld : AbstractPhysicsWorld<PhysicsEntity, WorldCollisionData<PhysicsEntity>>(){
     override fun createCollisionData(pair: CollisionPair<CollisionItem<PhysicsEntity, BodyFixture>>?): WorldCollisionData<PhysicsEntity>? {
         return null;
     }
-    fun getDrawables() : List<DrawableThing>{
-        return emptyList()
-    }
 }
 
-abstract class EffectsEntity : DrawableThing{}
+abstract class EffectsEntity : DrawableProvider{}
 
 class SimpleEffectsEntity(
     var position: Vector2,
@@ -85,20 +103,19 @@ class SimpleEffectsEntity(
     val model: Model,
 ) : EffectsEntity(){
 
-    override fun getTransformedComponents(): MutableList<Pair<Graphics.Model, Graphics.Transform>> {
-        return mutableListOf(Pair(model, Graphics.Transform(position, angle)))
-    }
-
-    override fun getRequiredModels(): MutableList<Graphics.Model> {
-        return mutableListOf(model)
+    //    override fun getTransformedComponents(): MutableList<Pair<Graphics.Model, Graphics.Transform>> {
+//        return mutableListOf(Pair(model, Graphics.Transform(position, angle)))
+//    }
+//
+//    override fun getRequiredModels(): MutableList<Graphics.Model> {
+//        return mutableListOf(model)
+//    }
+    override fun getDrawableInstances(): List<DrawableInstance> {
+        return return mutableListOf(DrawableInstance(model, Graphics.Transform(position.copy(), angle)))
     }
 
 }
 
 class EffectsWorld{
     val entities = listOf<EffectsEntity>();
-    fun getDrawables() : List<DrawableThing>{
-        return entities;
-    }
-
 }
