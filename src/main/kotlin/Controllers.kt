186daf0953f2e785b4924ecc3f26afc2e0318f1a
Ewan Brown <ewanbrown2000@gmail.com>
2024-07-16 23:53:56 -0400

    Branched off Controller layer
    Controller is responsible for processing player input and AI logic based off externally provided (input) and world info.
    
    AI logic is to be targetted for a specific entity type, as the entity type will dictate what actions are available and that IS AS GENERIC AS WE'RE GOING WITH IT

---
+++ src/main/kotlin/Controllers.kt
@@ -0,0 +1,66 @@
+import org.dyn4j.geometry.Vector2
+import java.awt.event.KeyEvent
+import java.util.BitSet
+
+//For AI and player controls!
+class ControllerLayer : Layer{
+
+    abstract class Controller<in E : PhysicsEntity>(){
+        abstract fun update(entity : E)
+    }
+
+    fun <E : PhysicsEntity> addControlledEntity(entity: E, controller: Controller<E>){
+        val entry = ControllerEntityEntry(controller, entity)
+        controllerList.add(entry)
+    }
+
+    private data class ControllerEntityEntry<E : PhysicsEntity>(val controller: Controller<E>,  var entity: E){
+        fun update() {
+            controller.update(entity)
+        }
+    }
+    private val controllerList = mutableListOf<ControllerEntityEntry<*>>()
+
+    override fun update() {
+        controllerList.removeIf { t -> t.entity.isMarkedForRemoval() }
+        for (controllerEntityEntry in controllerList) {
+            controllerEntityEntry.update()
+        }
+    }
+
+    override fun populateModelMap(modelDataMap: HashMap<Graphics.Model, MutableList<Graphics.Transform>>) {
+        //Add renderables for player perspective?? Interesting idea
+    }
+}
+
+class PlayerController(val input: BitSet) : ControllerLayer.Controller<ShipEntity>(){
+    override fun update(entity: ShipEntity) {
+        var x = 0.0;
+        var y = 0.0;
+        if(input[KeyEvent.VK_W]){
+            y++;
+        }
+        if(input[KeyEvent.VK_S]){
+            y--;
+        }
+        if(input[KeyEvent.VK_D]){
+            x++;
+        }
+        if(input[KeyEvent.VK_A]){
+            x--;
+        }
+
+        if(input[KeyEvent.VK_SPACE]){
+            x = entity.linearVelocity.x
+            y = entity.linearVelocity.y
+        }
+
+        if(input[KeyEvent.VK_SHIFT]){
+            x *= 2;
+            y *= 2;
+        }
+
+        entity.applyForce(Vector2(x,y))
+
+    }
+}
