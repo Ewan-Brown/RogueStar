804170a2ca88100747f6d682b5d46be5bcbdf3a5
Ewan-Brown <ewanbrown2000@gmail.com>
2024-08-10 11:39:49 -0400

    Cleaned up the thruster particle effects

--- src/main/kotlin/Effects.kt
+++ src/main/kotlin/Effects.kt
@@ -5,17 +5,17 @@ class EffectsUtils{
     companion object {
         fun emitThrustParticles(entity: PhysicsEntity, thrust: Vector2) {
             if (thrust.magnitude > 0) {
-                val adjustedThrust = thrust.product(-0.01).rotate((Math.random() - 0.5) / 3)
+                val adjustedThrust = thrust.product(-0.002).rotate((Math.random() - 0.5) / 3)
                 effectsLayer.addEntity(
                     ParticleEntity(
                         Model.SQUARE1,
                         entity.worldCenter.x,
                         entity.worldCenter.y,
                         Math.random(),
-                        graphicsDataProvider = {it -> GraphicalData(it.getLife(),0.0f,0.0f,-10.0f)},
+                        graphicsDataProvider = {it -> GraphicalData(it.getLife(),0.0f,0.0f,10-it.getLife())},
                                 dx = entity.changeInPosition.x + adjustedThrust.x
                         ,dy = entity.changeInPosition.y + adjustedThrust.y,
-                        //drotation = (Math.random() - 0.5) * 10
+                        drotation = (Math.random() - 0.5)
                     )
                 )
             }
@@ -55,7 +55,7 @@ abstract class EffectsEntity{
 class ParticleEntity(val model: Model, var x : Double, var y : Double, var rotation : Double,
                      val graphicsDataProvider: (ParticleEntity) -> GraphicalData, var dx : Double = 0.0, var dy : Double = 0.0, var drotation : Double = 0.0, var scale: Double = 1.0) : EffectsEntity() {
 
-    private val MAX_LIFE: Int = 1000
+    private val MAX_LIFE: Int = 100
     var lifetime: Int = MAX_LIFE
     var isDead = false;
 
@@ -67,7 +67,7 @@ class ParticleEntity(val model: Model, var x : Double, var y : Double, var rotat
         val components = mutableListOf<RenderableComponent>()
 
         val entityPos = Vector2(x, y)
-        val variableScale = Math.min(scale, Vector2(dx, dy).magnitude*10);
+        val variableScale = getLife().toDouble();
         if(variableScale < 0.01){
             isDead = true
         }
@@ -81,9 +81,9 @@ class ParticleEntity(val model: Model, var x : Double, var y : Double, var rotat
     }
     override fun update() {
         lifetime--
-        x += dx// * (lifetime/MAX_LIFE)
-        y += dy// * (lifetime/MAX_LIFE)
-        rotation += drotation//  * (lifetime/MAX_LIFE)
+        x += dx * getLife()
+        y += dy * getLife()
+        rotation += drotation * getLife()
     }
     override fun isMarkedForRemoval(): Boolean{
         return lifetime < 0 || isDead
