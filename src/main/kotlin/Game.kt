1655354c6e70450c3e97d6dbc2066e59d1afa704
Ewan Brown <ewanbrown2000@gmail.com>
2024-09-08 21:15:12 -0400

    Broken components now also create a effects entity (which looks identical to the component) and get removed from physics

--- src/main/kotlin/Game.kt
+++ src/main/kotlin/Game.kt
@@ -55,7 +55,7 @@ fun main() {
 
     val greenEntities = mutableListOf<PhysicsEntity>()
     val blueEntities = mutableListOf<PhysicsEntity>()
-    for(i in 1..10){
+    for(i in 1..1){
         greenEntities.add(physicsLayer.addEntity(ShipEntity(1.0, 0.0f, 1.0f, 0.0f, greenTeam), 0.0, Vector2(Math.random()-0.5, Math.random() - 0.5).multiply(30.0)))
 
     }
