ae654193933aafbc8583c0ee08cd426d9f2462ab
Ewan Brown <ewanbrown2000@gmail.com>
2024-09-24 12:35:48 -0400

    Privatized physics stuff

--- src/main/kotlin/Controllers.kt
+++ src/main/kotlin/Controllers.kt
@@ -3,47 +3,48 @@ import org.dyn4j.geometry.Vector2
 import java.awt.event.KeyEvent
 import java.util.BitSet
 import kotlin.math.abs
+import PhysicsLayer.PhysicsBodyData
 
-//TODO Generalized PID utilities
 class ControllerLayer : Layer{
 
-    fun removeController(entity: PhysicsEntity){
-        controllerList.removeIf{ it -> it.input.any { it == entity}}
+    fun removeController(uuid: Int){
+        controllerList.removeIf {
+            it.input.removeIf { it2 -> it2.uuid == uuid } //Remove this entry from the input list
+            it.input.size == 0 //If that was the last entry for this controller, remove the controller too :)
+        }
     }
 
-    fun calcTorqueToTurnTo(desiredAngleVec: Vector2, entity: PhysicsEntity) : Double{
-        val angleDiff = desiredAngleVec.getAngleBetween(entity.transform.rotationAngle)
-        return (-angleDiff*8 - entity.angularVelocity*4) * entity.mass.mass
+    private fun calcTorqueToTurnTo(desiredAngleVec: Vector2, angle: Double, angularVelocity: Double) : Double{
+        val angleDiff = desiredAngleVec.getAngleBetween(angle)
+        return (-angleDiff*8 - angularVelocity*4)
     }
 
-    fun doPositionalControl(entity: PhysicsEntity, targetPos: Vector2, targetOrientation: Vector2 = entity.worldCenter.to(targetPos), targetVelocity: Vector2 = Vector2()){
-        val velocityVector = entity.linearVelocity
-        val velocityDiff = velocityVector.to(targetVelocity)
-        val vecToTarget = entity.worldCenter.to(targetPos)
+    private fun doPositionalControl(data: PhysicsLayer.PhysicsBodyData, targetPos: Vector2, targetOrientation: Vector2, targetVelocity: Vector2 = Vector2()){
+        val velocityDiff: Vector2 = data.velocity.to(targetVelocity)
+        val vecToTarget = data.position.to(targetPos)
 
         val calcThrustToGetTo : (Vector2) -> Vector2 = fun(desiredPosition) : Vector2{
-            val posDiff = desiredPosition.difference(entity.worldCenter)
+            val posDiff = desiredPosition.difference(data.position)
             return Vector2(posDiff.multiply(8.0)).add(velocityDiff.product(4.0))
         }
 
         val thrust : Vector2 = calcThrustToGetTo(targetPos)
-        val torque : Double = calcTorqueToTurnTo(targetOrientation, entity)
+        val torque : Double = calcTorqueToTurnTo(targetOrientation, data.angle, data.angularVelocity)
 
-
-        entity.applyTorque(torque)
-        entity.applyForce(thrust)
-        emitThrustParticles(entity, thrust)
+        physicsLayer.applyForce(data.uuid, thrust)
+        physicsLayer.applyTorque(data.uuid, torque)
+        emitThrustParticles(data, thrust)
     }
 
-    abstract class BaseController<E : PhysicsEntity>{
+    abstract class BaseController<E : PhysicsBodyData>{
         abstract fun update(input: List<E>)
     }
 
-    abstract class Controller<E : PhysicsEntity> : BaseController<E>() {
+    abstract class Controller<E : PhysicsBodyData> : BaseController<E>() {
         abstract override fun update(input : List<E>)
     }
 
-    abstract class SingleController<E : PhysicsEntity> : Controller<E>(){
+    abstract class SingleController<E : PhysicsBodyData> : Controller<E>(){
         final override fun update(input: List<E>) {
             if(input.size != 1){
                 throw IllegalArgumentException("A $javaClass should only ever be attached to one entity at a time ")
@@ -54,7 +55,7 @@ class ControllerLayer : Layer{
         abstract fun update(input: E);
     }
 
-    inner class BubbleMultiController<E : PhysicsEntity>(inline val centerGenerator: () -> Vector2 , val radius: Double, var bubbleVelocity: Vector2 = Vector2()) : Controller<E>(){
+    inner class BubbleMultiController<E : PhysicsBodyData>(inline val centerGenerator: () -> Vector2 , val radius: Double, var bubbleVelocity: Vector2 = Vector2()) : Controller<E>(){
 
         val bubbleAnchorMap = mutableMapOf<Int, Vector2>()
         val inBubbleTrackerMap = mutableMapOf<Int, Boolean>()
@@ -63,37 +64,49 @@ class ControllerLayer : Layer{
             bubbleAnchorMap[index] = Vector2(radius*0.5 + Math.random()*radius*0.2,0.0).rotate(Math.random()*2*Math.PI)
         }
 
-        override fun update(entities: List<E>) {
+        override fun update(data: List<E>) {
             if(bubbleAnchorMap.isEmpty()){
-                for(entity in entities){
-                    val index = entity.uuid
+                for(datum in data){
+                    val index = datum.uuid
                     inBubbleTrackerMap[index] = false
                     randomizeAnchor(index)
                 }
             }
-            for (entity in entities) {
-                val index = entity.uuid
-                val vecToBubbleCenter = entity.worldCenter.to(centerGenerator())
-                if(abs(vecToBubbleCenter.magnitude) < radius){
-                    inBubbleTrackerMap[index] = true
-                    doPositionalControl(entity, centerGenerator().sum(bubbleAnchorMap[index]), Vector2(entity.transform.rotationAngle), bubbleVelocity)
-                }else{
-                    if(inBubbleTrackerMap[index]!!){
-                        inBubbleTrackerMap[index] = false
-                        randomizeAnchor(index)
+            for (datum in data) {
+                if(datum.position != null) {
+                    val index = datum.uuid
+                    val vecToBubbleCenter = datum.position.to(centerGenerator())
+                    if (abs(vecToBubbleCenter.magnitude) < radius) {
+                        inBubbleTrackerMap[index] = true
+                        doPositionalControl(
+                            datum,
+                            centerGenerator().sum(bubbleAnchorMap[index]),
+                            Vector2(datum.angle),
+                            bubbleVelocity
+                        )
+                    } else {
+                        if (inBubbleTrackerMap[index]!!) {
+                            inBubbleTrackerMap[index] = false
+                            randomizeAnchor(index)
+                        }
+                        doPositionalControl(
+                            datum,
+                            centerGenerator().sum(bubbleAnchorMap[index]),
+                            Vector2(datum.angle),
+                            bubbleVelocity
+                        )
                     }
-                    doPositionalControl(entity, centerGenerator().sum(bubbleAnchorMap[index]), Vector2(entity.transform.rotationAngle), bubbleVelocity)
                 }
             }
         }
 
     }
 
-    fun <E : PhysicsEntity> addControllerEntry(group: List<E>, controller: Controller<E>){
+    fun <E : PhysicsBodyData> addControllerEntry(group: MutableList<E>, controller: Controller<E>){
         controllerList.add(ControllerInputEntry(controller, group))
     }
 
-    private data class ControllerInputEntry<E : PhysicsEntity>(val controller: BaseController<E>, var input: List<E>){
+    private data class ControllerInputEntry<E : PhysicsBodyData>(val controller: BaseController<E>, var input: MutableList<E>){
         fun update(){
             controller.update(input)
         }
@@ -101,20 +114,11 @@ class ControllerLayer : Layer{
 
 
     private val controllerList = mutableListOf<ControllerInputEntry<*>>()
-    private val entityRequestBuffer = mutableListOf<PhysicsEntity>()
 
     override fun update() {
-        entityRequestBuffer.clear()
         for (controllerEntityEntry in controllerList) {
             controllerEntityEntry.update()
         }
-        for (physicsEntity in entityRequestBuffer) {
-            physicsLayer.addEntity(physicsEntity)
-        }
-    }
-
-    fun getNewEntityRequests() : List<PhysicsEntity>{
-        return entityRequestBuffer
     }
 
     fun populateModelMap(modelDataMap: HashMap<Graphics.Model, MutableList<Pair<Transformation, GraphicalData>>>) {
@@ -122,8 +126,8 @@ class ControllerLayer : Layer{
     }
 }
 
-class PlayerController(val input: BitSet) : ControllerLayer.SingleController<ShipEntity>(){
-    override fun update(entity: ShipEntity) {
+class PlayerController(val input: BitSet) : ControllerLayer.SingleController<PhysicsBodyData>(){
+    override fun update(entityData: PhysicsBodyData) {
         var x = 0.0
         var y = 0.0
         var r = 0.0
@@ -152,29 +156,28 @@ class PlayerController(val input: BitSet) : ControllerLayer.SingleController<Shi
         var needsRotation = true
 
         if(input[KeyEvent.VK_SPACE]){
-            x = -entity.linearVelocity.x/10.0
-            y = -entity.linearVelocity.y/10.0
+            x = -entityData.velocity.x/10.0
+            y = -entityData.velocity.y/10.0
             needsRotation = false
         }
 
         if(input[KeyEvent.VK_X]){
-            //TODO Maybe make this a little more abstracted, I don't like having to directly affect kinematics from this layer when we can avoid it...
-            val newEntity = ProjectileEntity(entity.team)
-            val addedEntity = physicsLayer.addEntity(newEntity, entity.transform.rotationAngle, entity.worldCenter.sum(Vector2(entity.transform.rotation.toVector().product(entity.rotationDiscRadius+1))))
-            addedEntity.linearVelocity = entity.linearVelocity.copy()
+//            val newEntity = ProjectileEntity(entity.team)
+//            val addedEntity = physicsLayer.addEntity(newEntity, entity.transform.rotationAngle, entity.worldCenter.sum(Vector2(entity.transform.rotation.toVector().product(entity.rotationDiscRadius+1))))
+//            addedEntity.linearVelocity = entity.linearVelocity.copy()
         }
 
         val desiredVelocity = Vector2(x, y)
         if(needsRotation){
-            desiredVelocity.rotate(entity.transform.rotation)
+            desiredVelocity.rotate(entityData.angle)
         }
 
         val thrust = desiredVelocity.product(100.0)
 
-        emitThrustParticles(entity, thrust)
-        entity.applyForce(thrust)
+        emitThrustParticles(entityData, thrust)
+        physicsLayer.applyForce(entityData.uuid, thrust)
 
-        val rotate = r*5 - entity.angularVelocity
-        entity.applyTorque(rotate*10.0)
+        val rotate = r*5 - entityData.angularVelocity
+        physicsLayer.applyTorque(entityData.uuid, rotate*10.0)
     }
 }
