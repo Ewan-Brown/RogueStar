d10ae031893c06fd662f3f46c21268b57c46a7c9
Ewan-Brown <ewanbrown2000@gmail.com>
2024-11-25 20:35:20 -0500

    Added Vector2 Utils

--- src/main/kotlin/Util.kt
+++ src/main/kotlin/Util.kt
@@ -2,3 +2,8 @@ import org.dyn4j.geometry.Vector2
 
 operator fun Vector2.component1() = this.x
 operator fun Vector2.component2() = this.y
+
+operator fun Vector2.plus(vec: Vector2): Vector2 = this.sum(vec)
+operator fun Vector2.minus(vec: Vector2): Vector2 = this.difference(vec)
+operator fun Vector2.times(factor: Double): Vector2 = this.multiply(factor)
+operator fun Vector2.div(factor: Double): Vector2 = this.quotient(factor)
