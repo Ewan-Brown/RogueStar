b03a76390d9ed0968b13ac2a14bec2c4bb535807
Ewan Brown <Ewan.Brown@Callminer.com>
2025-04-20 19:53:45 -0400

    Fixed issue where fixtures were being rendered even if dead

--- src/main/kotlin/GraphicsService.kt
+++ src/main/kotlin/GraphicsService.kt
@@ -13,34 +13,39 @@ class GraphicsService {
     fun <T : PhysicsLayer.BasicFixture> componentToRenderable(entity : PhysicsLayer.PhysicsEntity, fixtureSlot : PhysicsLayer.PhysicsEntity.FixtureSlot<T>) : Graphics.RenderableEntity? {
 
         entity.run {
-            val entityAngle = getTransform().rotation.toRadians()
-            val entityPos = this.worldCenter
-            val absolutePos = fixtureSlot.localTransform.translation.toVec2().subtract(this.getMass().center)
-                .rotate(entityAngle).add(entityPos)
-            val newAngle =
-                Rotation(fixtureSlot.localTransform.rotation.toRadians() + this.transform.rotationAngle)
-            val scale = fixtureSlot.localTransform.scale
+            val fixture = entity.fixtureSlotFixtureMap[fixtureSlot]
+            if(fixture != null){
+                val entityAngle = getTransform().rotation.toRadians()
+                val entityPos = this.worldCenter
+                val absolutePos = fixtureSlot.localTransform.translation.toVec2().subtract(this.getMass().center)
+                    .rotate(entityAngle).add(entityPos)
+                val newAngle =
+                    Rotation(fixtureSlot.localTransform.rotation.toRadians() + this.transform.rotationAngle)
+                val scale = fixtureSlot.localTransform.scale
 
-            return when(fixtureSlot){
-                is PhysicsLayer.PhysicsEntity.ThrusterFixtureSlot -> {
-                    Graphics.RenderableEntity(
-                        fixtureSlot.model,
-                        Transformation(absolutePos.toVec3(), scale, newAngle),
-                        Graphics.ColorData(1.0f, 1.0f, 0.0f, 0.0f),
-                        Graphics.MetaData(1.0f)
-                    )
+
+                return when(fixtureSlot){
+                    is PhysicsLayer.PhysicsEntity.ThrusterFixtureSlot -> {
+                        Graphics.RenderableEntity(
+                            fixtureSlot.model,
+                            Transformation(absolutePos.toVec3(), scale, newAngle),
+                            Graphics.ColorData(1.0f, 1.0f, 0.0f, 0.0f),
+                            Graphics.MetaData(1.0f)
+                        )
+                    }
+                    is PhysicsLayer.PhysicsEntity.BasicFixtureSlot -> {
+                        Graphics.RenderableEntity(
+                            fixtureSlot.model,
+                            Transformation(absolutePos.toVec3(), scale, newAngle),
+                            Graphics.ColorData(0.0f, 0.0f, 1.0f, 0.0f),
+                            Graphics.MetaData(1.0f)
+                        )
+                    }
                 }
-                is PhysicsLayer.PhysicsEntity.BasicFixtureSlot -> {
-                    Graphics.RenderableEntity(
-                        fixtureSlot.model,
-                        Transformation(absolutePos.toVec3(), scale, newAngle),
-                        Graphics.ColorData(0.0f, 0.0f, 1.0f, 0.0f),
-                        Graphics.MetaData(1.0f)
-                    )
-                }
+            }else{
+                return null;
+
             }
-
-
         }
     }
 }
