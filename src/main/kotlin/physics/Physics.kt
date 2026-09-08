6afbc8517f317eb22bb338c1adf942527f4cfc29
Ewan Brown <ewanbrown2000@gmail.com>
2026-09-07 21:46:45 -0400

    Removed "world" layer

--- src/main/kotlin/physics/Physics.kt
+++ src/main/kotlin/physics/Physics.kt
@@ -10,10 +10,13 @@ import graphics.WHITE
 import graphics.processRenderables
 import math.Coordinates
 import math.EntityReferenceFrame
+import math.HasReferenceFrame
+import math.Pose
 import math.ReferenceFrame
 import math.ReferenceFrameVariable
 import math.Transform
 import math.Vector2
+import math.WorldReferenceFrame
 import math.getTransformLocalToParentFrame
 import models.Model
 import kotlin.collections.HashMap
@@ -31,13 +34,79 @@ value class Timestamp(val time: Double){
 @JvmInline
 value class TimeDuration(val duration: Double)
 
-class PhysicsManager() : EntityConsumer {
-    fun update(timeStep: Double) {
-        world.update(timeStep)
+class PhysicsManager() : EntityConsumer, HasReferenceFrame<WorldReferenceFrame> {
+
+    private val entities = mutableListOf<KineticEntity>()
+    private val entityBuffer = mutableListOf<KineticEntity>()
+
+    fun update(timestep: Double) {
+        synchronized(entityBuffer){
+            entities.addAll(entityBuffer)
+            entityBuffer.clear()
+        }
+
+        // Do entity physics updates
+        for (entity in entities) {
+            //Calculate new positions
+            var velocity = entity.getVelocity()
+            var rotVelocity = entity.getRotationalVelocity()
+
+            val comLocal = entity.getCenterOfMass()
+            var comWorld = comLocal.applyTransform(getTransformLocalToParentFrame(entity))
+
+            entity.rotate(rotVelocity * timestep)
+            comWorld += velocity * timestep
+            entity.setPose(Pose(Coordinates(comWorld.getVector() - comLocal.getVector().rotate(entity.getOrientation().getAngle())), entity.getOrientation(), entity.getZHeight()))
+
+            val entityAngle = entity.getOrientation().getAngle()
+
+            //Calculate new derivatives
+            velocity = entity.getVelocity() + if(entity.getMass() > 0) entity.popNetForce().rotate(entityAngle)/entity.getMass() else Vector2()
+            rotVelocity = entity.getRotationalVelocity() + entity.popNetTorque()/entity.getMass()
+
+            //Apply friction
+            velocity *= 0.99
+            rotVelocity *= 0.99
+            entity.setVelocity(velocity)
+            entity.setRotationalVelocity(rotVelocity)
+        }
+
+        for(projectile in entities.filterIsInstance<ProjectileEntity>()){
+            val pointOfContactDescriptor = projectile.getProjectileInteractionDescriptor()
+            when(pointOfContactDescriptor){
+                is LineProjectileInteraction -> println("line projectile interaction not defined!")
+                is PointProjectileInteraction -> println("point projectile interaction not defined!")
+                is RadiusProjectileInteraction -> TODO()
+                is DisabledInteraction -> {}  // do nothing!
+            }
+//            val pointOfContactLocal = projectile.getPointOfContact()
+//            val pointOfContactWorld = pointOfContactLocal.rotate(projectile) + projectile.getWorldTransform().translation
+//            for(entity in entities){
+//                if(entity != projectile){
+//                    //TODO Do cheap preliminary collision checking
+//                }
+//            }
+        }
+
+        //Do pawn updates
+        for(entity in entities) {
+            if(entity is ComplexEntity){
+                for(pawn in entity.getPawnsInside()){
+                    pawn.translate(pawn.getVelocity())
+//                pawn.setVelocity(Vector2())
+                }
+            }
+        }
+
+        // Do entity updates
+        for (entity in entities) {
+            entity.update(timestep)
+        }
+        entities.removeIf{it.markedForRemoval()}
     }
 
     fun populateModelMap(modelDataMap: HashMap<Model, MutableList<Renderer.Renderable>>) {
-        for (entity in world.getEntities()) {
+        for (entity in entities) {
             //TODO replace with processor call
                 processRenderables(entity, {
                     modelDataMap[it.model]!!.add(it)
@@ -48,7 +117,7 @@ class PhysicsManager() : EntityConsumer {
     fun getDebugLines(): List<DebugLineData> {
         val lines = mutableListOf<DebugLineData>()
 
-        for (entity in world.getEntities()) {
+        for (entity in entities) {
             val pos = entity.getCoordinates()
             val com = entity.getCenterOfMass().applyTransform(getTransformLocalToParentFrame(entity))
             lines.add(DebugLineData(com, pos, CYAN, BLUE))
@@ -63,11 +132,11 @@ class PhysicsManager() : EntityConsumer {
     }
 
     override fun addEntity(entity: KineticEntity) {
-        world.addEntity(entity)
-        entity.setEntityConsumer(this)
+        synchronized(entityBuffer){
+            entityBuffer.add(entity)
+            entity.setEntityConsumer(this)
+        }
     }
-
-    val world: World = FlatWorld() //In theory this is so I can replace this with non-flat worlds easily... Not sure about that...
 }
 
 data class Force<S : ReferenceFrame>(val vector: Vector2, val origin: Coordinates<S>) : ReferenceFrameVariable<S>{
